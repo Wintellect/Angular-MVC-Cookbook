7853dd59fc2e60bd30a3a5ea122fd93c0498bdde
Dave Baskin <dbaskin@wintellect.com>
2013-02-02 17:54:31 -0500

    Adding Bootstrap files.

--- BasicProject/MvcAngular.Web/App_Start/BundleConfig.cs
+++ BasicProject/MvcAngular.Web/App_Start/BundleConfig.cs
@@ -8,36 +8,24 @@ namespace MvcAngular.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // CSS Bundles
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/css-one")
+                .Include("~/Content/bootstrap/bootstrap.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new StyleBundle("~/Content/css-two")
+                .Include("~/Content/bootstrap/bootstrap-responsive.css")
+                .Include("~/Content/app/main.css"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            // Script Bundles
 
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Scripts/bootstrap/modernizr-2.6.2-respond-1.1.0.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .Include("~/Scripts/jquery/jquery-{version}.js")
+                .Include("~/Scripts/bootstrap/bootstrap.js"));
         }
     }
 }
