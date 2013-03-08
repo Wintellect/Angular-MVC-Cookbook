6d279db898988dff3fee53c1c1d7ff4d25a6e7eb
Dave Baskin <dbaskin@wintellect.com>
2013-03-07 19:53:24 -0500

    Upgrading to AngularJS v1.1.3. Fixing error in Testacular unit test configuration file.

--- CRUDOperations/MvcAngular.Web/Scripts/testing/config/testacular-unit-tests.conf.js
+++ CRUDOperations/MvcAngular.Web/Scripts/testing/config/testacular-unit-tests.conf.js
@@ -4,13 +4,6 @@ files = [
     JASMINE,
     JASMINE_ADAPTER,
     'angular/angular.js',
-    'angular/angular-bootstrap.js',
-    'angular/angular-bootstrap-prettify.js',
-    'angular/angular-cookies.js',
-    'angular/angular-loader.js',
-    'angular/angular-resource.js',
-    'angular/angular-sanitize.js',
-    'angular/angular-scenario.js',
     'angular/angular-mocks.js',
     'app/**/*.js',
     'testing/unit-tests/**/*.js'
